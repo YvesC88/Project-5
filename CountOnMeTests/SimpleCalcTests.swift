9e22c7a3e9540b3e1d96a2106acc7d2fdf176e6c
Yves Charpentier <charpentier.yves@live.fr>
2021-09-07 18:14:38 +0200

    Setup project

---
+++ CountOnMeTests/SimpleCalcTests.swift
@@ -0,0 +1,13 @@
+//
+//  SimpleCalcTests.swift
+//  SimpleCalcTests
+//
+//  Created by Vincent Saluzzo on 29/03/2019.
+//  Copyright © 2019 Vincent Saluzzo. All rights reserved.
+//
+
+import XCTest
+@testable import CountOnMe
+
+class SimpleCalcTests: XCTestCase {
+}
---
+++ CountOnMeUITests/SimpleCalcUITests.swift
@@ -0,0 +1,13 @@
+//
+//  SimpleCalcUITests.swift
+//  SimpleCalcUITests
+//
+//  Created by Vincent Saluzzo on 29/03/2019.
+//  Copyright © 2019 Vincent Saluzzo. All rights reserved.
+//
+
+import XCTest
+
+class SimpleCalcUITests: XCTestCase {
+    
+}
